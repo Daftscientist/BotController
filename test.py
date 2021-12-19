5d25d9d9823327c6b76697ee0122bfcbcbc42391
Daftscientist <leojohnston12032@gmail.com>
2021-12-19 18:20:52 +0000

    Turned some stuff async.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ client = Client(intents=Intents.ALL)
 commands = handler.App(client, "!", case_sensitive=True)
 
 @commands.command(command_name="help")
-def help_command(message):
-    return message.channel.send(content="pong")
+async def help_command(message):
+    return await message.channel.send(content="pong")
 
 client.run("OTE4NTg4NzE1Mjg1MjMzNjg0.YbJcaA.N0wp8VZwwXMoEpgOyEmHoxzHjtE")
